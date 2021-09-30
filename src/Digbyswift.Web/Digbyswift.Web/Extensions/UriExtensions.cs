026113ca1b571651a6e940dd190be886dc6fe03f
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2021-09-30 16:51:52 +0100

    Add initial 4.6.2 targeted files

---
+++ src/Digbyswift.Web/Digbyswift.Web/Extensions/UriExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace Digbyswift.Web.Extensions
+{
+    public static class UriExtensions
+    {
+        public static string ReplaceQueryString(this Uri uri, object newValues)
+        {
+            var values = new RouteValueDictionary(newValues);
+            var query = HttpUtility.ParseQueryString(uri.Query);
+            foreach (var key in values.Keys)
+            {
+                query[key] = values[key].ToString();
+            }
+
+            var builder = new UriBuilder(uri)
+            {
+                Query = string.Join("&", query.AllKeys.Select(x => string.Join("=", x, query[(string)x].ToString())))
+            };
+            return builder.ToString();
+        }
+    }
+}
---
+++ src/Digbyswift.Web/Digbyswift.Web/Mvc/ModelBinders/AliasModelBinder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.Mvc;
+using Digbyswift.Web.Mvc.Attributes;
+
+namespace Digbyswift.Web.Mvc.ModelBinders
+{
+    public class AliasModelBinder : DefaultModelBinder
+    {
+        protected override PropertyDescriptorCollection GetModelProperties(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            var toReturn = base.GetModelProperties(controllerContext, bindingContext);
+
+            var additional = new Collection<PropertyDescriptor>();
+
+            foreach (var p in GetTypeDescriptor(controllerContext, bindingContext).GetProperties().Cast<PropertyDescriptor>())
+            {
+                foreach (var attr in p.Attributes.OfType<BindAliasAttribute>())
+                {
+                    additional.Add(new BindAliasAttribute.AliasedPropertyDescriptor(attr.Alias, p));
+
+                    if (bindingContext.PropertyMetadata.ContainsKey(p.Name))
+                    {
+                        bindingContext.PropertyMetadata.Add(attr.Alias, bindingContext.PropertyMetadata[p.Name]);
+                    }
+                }
+            }
+
+            return new PropertyDescriptorCollection(toReturn.Cast<PropertyDescriptor>().Concat(additional).ToArray());
+        }
+    }
+}
